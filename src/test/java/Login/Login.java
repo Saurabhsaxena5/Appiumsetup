f0a2e1eefbcb2812aaa3f27cb51f2330a64a2843
Saurabh Saxena <142875358+Saurabhsaxena5@users.noreply.github.com>
2026-01-15 17:57:02 +0530

    Loginwithphonenumer

--- src/test/java/Login/Login.java
+++ src/test/java/Login/Login.java
@@ -5,6 +5,7 @@ import io.appium.java_client.AppiumBy;
 
 import java.time.Duration;
 
+import org.jspecify.annotations.Nullable;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -13,65 +14,90 @@ import org.testng.annotations.Test;
 
 public class Login extends BaseTest {
 
-    WebDriverWait wait;
+	WebDriverWait wait;
 
-    @Test
-    public void login() throws InterruptedException {
+	@Test
+	public void login() throws InterruptedException {
 
-    
-       Thread.sleep(5000);
-        String source = driver.getPageSource();
-        System.out.println(source);
+		Thread.sleep(5000);
+		String source = driver.getPageSource();
+		System.out.println(source);
 
-        
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
 
-       
-        WebElement drama = wait.until(
-                ExpectedConditions.presenceOfElementLocated(
-                        AppiumBy.accessibilityId("Drama")
-                )
-        );
-        drama.click();
+		WebElement drama = wait.until(ExpectedConditions.presenceOfElementLocated(AppiumBy.accessibilityId("Drama")));
+		drama.click();
 
-     
-        WebElement elementcheck = wait.until(
-                ExpectedConditions.elementToBeClickable(
-                        By.xpath("//android.widget.EditText")
-                )
-        );
-        elementcheck.click();
-        elementcheck.sendKeys("8920689888");
+		WebElement elementcheck = wait
+				.until(ExpectedConditions.elementToBeClickable(By.xpath("//android.widget.EditText")));
+		elementcheck.click();
+		elementcheck.sendKeys("8920689888");
 
-        // click Send OTP
-        WebElement clickonsendotp = wait.until(
-                ExpectedConditions.elementToBeClickable(
-                        By.xpath("//android.view.View[@content-desc='Send OTP']")
-                )
-        );
-        clickonsendotp.click();
+		// click Send OTP
+		WebElement clickonsendotp = wait.until(
+				ExpectedConditions.elementToBeClickable(By.xpath("//android.view.View[@content-desc='Send OTP']")));
+		clickonsendotp.click();
 
-        System.out.println("Application login successful");
+		System.out.println("Application login successful");
 
-        // enter OTP
-        WebElement clickonelement = wait.until(
-                ExpectedConditions.elementToBeClickable(
-                        By.xpath("//android.widget.EditText")
-                )
-        );
-        clickonelement.click();
-        clickonelement.sendKeys("1234");
+		// enter OTP
+		WebElement clickonelement = wait
+				.until(ExpectedConditions.elementToBeClickable(By.xpath("//android.widget.EditText")));
+		clickonelement.click();
+		clickonelement.sendKeys("1234");
 
-        // click Verify OTP
-        WebElement clickonverifyotp = wait.until(
-                ExpectedConditions.elementToBeClickable(
-                        By.xpath("//android.view.View[@content-desc='Verify OTP']")
-                )
-        );
-        clickonverifyotp.click();
+		// click Verify OTP
+		WebElement clickonverifyotp = wait.until(
+				ExpectedConditions.elementToBeClickable(By.xpath("//android.view.View[@content-desc='Verify OTP']")));
+		clickonverifyotp.click();
 
-        Thread.sleep(5000);
+		Thread.sleep(5000);
 
-        System.out.println("Application login successfully");
-    }
+		System.out.println("Application login successfully");
+	}
+
+	@Test
+	public void ValidatewrongNumber() throws InterruptedException {
+
+		Thread.sleep(5000);
+		String source = driver.getPageSource();
+		System.out.println(source);
+
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+
+		WebElement drama = wait.until(ExpectedConditions.presenceOfElementLocated(AppiumBy.accessibilityId("Drama")));
+		drama.click();
+
+		WebElement elementcheck = wait
+				.until(ExpectedConditions.elementToBeClickable(By.xpath("//android.widget.EditText")));
+		elementcheck.click();
+		elementcheck.sendKeys("9634420847");
+
+		// click Send OTP
+		WebElement clickonsendotp = wait.until(
+				ExpectedConditions.elementToBeClickable(By.xpath("//android.view.View[@content-desc='Send OTP']")));
+		clickonsendotp.click();
+
+		// enter OTP
+		WebElement clickonelement = wait
+				.until(ExpectedConditions.elementToBeClickable(By.xpath("//android.widget.EditText")));
+		clickonelement.click();
+		clickonelement.sendKeys("1234");
+
+		// click Verify OTP
+		WebElement clickonverifyotp = wait.until(
+				ExpectedConditions.elementToBeClickable(By.xpath("//android.view.View[@content-desc='Verify OTP']")));
+		clickonverifyotp.click();
+
+		@Nullable
+		WebElement elementisvisible = wait
+				.until(ExpectedConditions.elementToBeClickable(By.xpath("//android.widget.ScrollView")));
+
+		if (elementisvisible.isDisplayed()) {
+			System.out.println("If using wrong Number so not login");
+		} else {
+			System.out.println("Login successfull");
+		}
+
+	}
 }
